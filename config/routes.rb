cb05a7d8a1d461092305951e0a5b0a16efdb4083
Nick <nick@jackrussellsoftware.com>
2011-04-21 19:22:24 -0400

    Homes controller implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Reminders::Application.routes.draw do
   match '/auth/:provider/callback', :to => 'sessions#callback'
+  root :to => 'home#show'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
