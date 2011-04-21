ebf5fb01568cb3ba9fd543fb6baca0078eb570a0
Nick <nick@jackrussellsoftware.com>
2011-04-21 18:59:53 -0400

    Reminders project started.

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+
+gem 'mysql2'
+
+gem "haml", ">= 3.0.0"
+gem "haml-rails"
+gem "jquery-rails"
+gem "omniauth", "~> 0.2.0"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reminders::Application.config.secret_token = '2773663c43d643ab13e9f9cf26481f7c92e11e101b1622bc58872bf74e037fdafb59c60adbacf4427e693ec53a0b53e253b0d8ab3ce77916cb4f6b2e0825b805'
